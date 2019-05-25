03781145add5c724c6d4c92286e774d9ff53396d
g-otn <g_otn@hotmail.com>
2019-05-25 01:59:41 -0300

    Create Leaf and ParentNode class

---
+++ HuffmanVisualizer/ParentNode.cs
@@ -0,0 +1,18 @@
+﻿namespace HuffmanVisualizer
+{
+    class ParentNode : Node
+    {
+        public Node Left { get; }      // bit: 0
+
+        public Node Right { get; }     // bit: 1
+        
+        public int Weight { get; }
+
+        public ParentNode(ParentNode left, int weight, ParentNode right) 
+        {
+            Left = left;
+            Weight = weight;
+            Right = right;
+        }
+    }
+}
